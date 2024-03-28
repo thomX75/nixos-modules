921f0c3241d2c4cf52c3544277c7d844976bcf78
ThomX75 <thom75@tuta.io>
2024-03-28 14:36:14 +0000

    Added Conky-Smart-Check.

---
+++ Conky-Smart-Check/README.md
@@ -0,0 +1,23 @@
+This module checks the SMART status of your drives using ```services.smartd``` and saves it to the file ```/tmp/disk_status.txt``` for Conky to read. See conky-screenshots folder.
+
+The text color of the status changes according to the disk status read from the file and remains unchanged when the status is "PASSES". It changes to yellow for "WARNING" and red for "FAILED" status.
+
+You nee to change "your-user-name" to your user name. You also need to add this code to your Conky configuration to read from the file.
+```
+SMART Status:${alignr}${if_match "${exec cat /tmp/disk_status.txt}" == "PASSED"} PASSED\
+${else}${if_match "${exec cat /tmp/disk_status.txt}" == "WARNING"} ${color yellow}WARNING\
+${else}${if_match "${exec cat /tmp/disk_status.txt}" == "FAILED"}  ${color red}FAILED\
+${else} ${color white}Unknown${endif}${endif}${endif}${color}
+```
+
+To install Conky Smart Check on your NixOS system, copy smart-check.nix to your modules folder and add it to the imports in configuration.nix:
+
+```
+  # Import configuration modules
+  imports = [
+      ...
+      ./modules/smart-check.nix
+      ...
+    ];                      
+```
+Now rebuild your system: ```sudo nixos-rebuild switch```
---
+++ Conky-Smart-Check/smart-check.nix
@@ -0,0 +1,93 @@
+{ config, pkgs, ... }:
+
+{
+
+  users.users.your-user-name.extraGroups = [ "smart" ];
+  
+  # Enable SmartD service
+  services.smartd = {
+    enable = true;
+    autodetect = true;
+    notifications.x11 = {
+      enable = true;
+      display = ":${toString config.services.xserver.display}";
+    };
+  };
+
+  # Creates a smart checking script named /etc/scripts/smart-check.sh
+  # which generates output in /tmp/disk_status.txt for Conky to read.
+  environment.etc =
+  {
+    "scripts/smart-check.sh" =
+    {
+      text =
+      ''
+      #!/run/current-system/sw/bin/bash
+
+      # Function to check SMART status of a disk
+      check_disk_smart_status() {
+          disk=$1
+          smart_status=$(sudo smartctl -H $disk | grep "SMART overall-health self-assessment test result")
+          if [[ $smart_status == *"PASSED"* ]]; then
+              disk_status="PASSED"
+          elif [[ $smart_status == *"FAILED"* ]]; then
+              disk_status="FAILED"
+          else
+              smart_warning=$(sudo smartctl -l warning $disk | grep "SMART Health Status")
+              if [[ $smart_warning == *"Warning"* ]]; then
+                  disk_status="WARNING"
+              else
+                  disk_status="UNKNOWN"
+              fi
+          fi
+      }
+
+      # Get list of disks
+      disks=$(lsblk -o NAME -nldA)
+
+      # Variable to track overall status
+      overall_status="PASSED"
+
+      # Loop through each disk
+      for disk in $disks; do
+          check_disk_smart_status "/dev/$disk"
+          if [[ "$disk_status" == "FAILED" ]]; then
+              overall_status="FAILED"
+              break
+          elif [[ "$disk_status" == "WARNING" ]]; then
+              overall_status="WARNING"
+          fi
+      done
+
+      # Write overall status to file
+      echo "$overall_status" > /tmp/disk_status.txt
+
+      '';
+      mode = "0554";
+    };
+  };
+
+  # Creates service to run the script.
+  systemd.services."SmartCheck" = {
+    script = ''
+      #!/run/current-system/sw/bin/bash
+      set -eu
+      /run/current-system/sw/bin/bash /etc/scripts/smart-check.sh
+    '';
+    serviceConfig = {
+      Type = "oneshot";
+      User = "root";
+    };
+  };
+
+  # Enables timer to run smart-check.sh at boot and every 30 minutes after.
+  systemd.timers."SmartCheck" = {
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnBootSec = "15s";            # Delay to start after boot, adjust as needed
+      OnUnitActiveSec = "30m";      # Run every 30 minutes
+      Unit = "SmartCheck.service";
+    };
+  };
+
+}
