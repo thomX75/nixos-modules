8518854367f0b7d6d834e2cdc4e2afc668f99427
thomX75 <thom75@tuta.io>
2025-01-19 18:31:49 +0000

    Added simple Home-Backup module.

---
+++ Home-Backup/README.md
@@ -0,0 +1,21 @@
+Note: I've created this module to daily back up my NixOS configuration files in case of unnoticed breakage of any sort.
+The existing backup solution seemed overcomplicated and too large for a simple task like backing up dotfiles.
+
+This module creates backups of your selected data in home-backup.conf using a systemd timer.
+
+The systemd timer is configured to create a backup at 17:00 every day.
+You need to adjust it to run it at times or/and days that suit you needs.
+Apart from the schedule, you will need to configure all paths and the list of folders to back up to function as desired.
+
+To install the module on your NixOS system, copy it to your modules folder and add it to the imports in configuration.nix:
+
+```
+  # Import configuration modules
+  imports = [
+      ...
+      ./modules/home-backup.nix
+      ...
+    ];                      
+```
+
+And rebuild your system: ```sudo nixos-rebuild switch```
---
+++ Home-Backup/home-backup.nix
@@ -0,0 +1,83 @@
+# Home directory backuper at scheduled time.
+# https://github.com/thomX75/nixos-modules
+
+# TODO: this module needs some configuration replace "your-user-name" with you actual user name, adjust backupDisk, backupFolder, sourceFile and maxBackups for your needs
+# TODO: sourceFile is a list of folders or files to be back up see example: home-backup.conf
+# TODO: on the bottom configure systemd.timer to run on desired dated and times
+
+{ config, pkgs, ... }:
+
+{
+
+  # Creates systemd service.
+  systemd.services."home-backup" = {
+    description = "create backup based on home-backup.conf source list file";
+    script = ''
+      # Get the hostname
+      hostname=$(cat /etc/hostname)
+
+      # Define variables
+      backupDisk="/mnt/Your-Disk"
+      backupFolder="$backupDisk/Backup/Home-Backup-$hostname/"
+      sourceFile="/home/your-user-name/.dotfiles/configs/home-backup.conf"
+      dateStamp="$(date +'%Y.%m.%d-%H.%M')"
+      maxBackups=14
+      backupUser="your-user-name"
+      backupGroup="users"
+
+      # Create backup folder if it doesn't exist
+      mkdir -p "$backupFolder"
+
+      # Check if a backup has already been made today
+      if [ -n "$(find "$backupFolder" -name "*.tgz" -daystart -mtime 0)" ]; then
+        echo "A backup has already been made today."
+        exit 0
+      fi
+
+      # Initialize an empty temporary file list
+      tempFileList=$(mktemp)
+
+      # Read each line from sourceFile and check if it exists
+      while IFS= read -r filePath; do
+        if [ -e "$filePath" ]; then
+          echo "$filePath" >> "$tempFileList"
+        else
+          echo "Skipping missing file or directory: $filePath"
+        fi
+      done < "$sourceFile"
+
+      # Compress files to temporary directory using tar
+      newArchive="$backupFolder$dateStamp.tgz"
+      PATH=/run/current-system/sw/bin:$PATH tar czf "$newArchive" -T "$tempFileList"
+
+      # Change ownership of the backup archive
+      chown -R "$backupUser:$backupGroup" "$backupFolder"
+
+      # Remove the temporary file list
+      rm "$tempFileList"
+
+      # Remove backups over $maxBackups
+      backupFiles=$(ls -t "$backupFolder" | tail -n +$(($maxBackups + 1)))
+      for file in $backupFiles; do
+        rm -- "$backupFolder$file"
+      done
+    '';
+    serviceConfig = {
+      Type = "oneshot";
+      User = "root";
+      StandardOutput = "journal+console";
+      StandardError = "journal+console";
+    };
+  };
+
+  # Enables timer to run home-backup service on calendar. 
+  systemd.timers."home-backup" = {
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnCalendar = "*-*-* 17:00";
+      Persistent = true;
+      Unit = "home-backup.service";
+    };
+  };
+
+}
